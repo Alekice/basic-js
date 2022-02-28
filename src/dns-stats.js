450908115a2785b3fd393852ee6f643011199be4
Aleksandra <semenovalekshu@gmail.com>
2022-02-28 11:54:35 +0300

    feat: solve dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let answer = {};
+  if (!domains.length) return answer;
+  
+  domains.forEach(domain => {
+    let words = domain.split('.').reverse();
+    let key = '';
+    for (let word of words) {
+      key += `.${word}`;
+      !answer[key] ? answer[key] = 1 : answer[key]++;
+    }
+    console.log(key);
+  });
+  
+  return answer;
 }
 
 module.exports = {
